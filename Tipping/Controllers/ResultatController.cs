492a7673fe6198a693115f1c1edbe313eec50d68
rasmusbauck <rasmus.bauck@gmail.com>
2012-05-28 08:31:15 +0200

    Results: added most/least points for a game

--- Tipping/Controllers/ResultatController.cs
+++ Tipping/Controllers/ResultatController.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using System.Web.Mvc;
 using Tipping.Data;
 using Tipping.Models;
@@ -18,6 +19,15 @@ namespace Tipping.Controllers
             viewModel.TotalPoengsum = perBruker.Select(bruker => new BrukerMedScore {Brukernavn = bruker.Key, Score = bruker.Sum(en => en.Poeng)}).OrderByDescending(bruker => bruker.Score).ToList();
             viewModel.KamperMedFirePoeng = perBruker.Select(bruker => new BrukerMedScore { Brukernavn = bruker.Key, Score = bruker.Count(en => en.Poeng == 4) }).OrderByDescending(bruker => bruker.Score).ToList();
             viewModel.KamperMedPoeng = perBruker.Select(bruker => new BrukerMedScore { Brukernavn = bruker.Key, Score = bruker.Count(en => en.Poeng > 0) }).OrderByDescending(bruker => bruker.Score).ToList();
+            var perKamp = tips.GroupBy(ettTips => ettTips.KampID);
+            var kamperMedPoeng = perKamp.Select(kamp => new KeyValuePair<int, int> (kamp.Key, kamp.Sum(en => en.Poeng))).OrderByDescending(en => en.Value);
+            var maxScore = kamperMedPoeng.Max(k => k.Value);
+            var minScore = kamperMedPoeng.Min(k => k.Value);
+
+            var maxKamper = kamperMedPoeng.Where(k => k.Value == maxScore);
+            viewModel.KampMedFlestPoeng = (from maxKamp in maxKamper let kamp = DataAksessor.HentKamp(maxKamp.Key) select new KampMedScore { Lag = kamp.Hjemmelag + " - " + kamp.Bortelag, Score = maxKamp.Value }).ToList();
+            var minKamper = kamperMedPoeng.Where(k => k.Value == minScore);
+            viewModel.KampMedFærrestPoeng = (from minKamp in minKamper let kamp = DataAksessor.HentKamp(minKamp.Key) select new KampMedScore { Lag = kamp.Hjemmelag + " - " + kamp.Bortelag, Score = minKamp.Value }).ToList();
 
             return View(viewModel);
         }
--- Tipping/Models/ResultatViewModel.cs
+++ Tipping/Models/ResultatViewModel.cs
@@ -7,6 +7,14 @@ namespace Tipping.Models
         public IEnumerable<BrukerMedScore> TotalPoengsum { get; set; }
         public IEnumerable<BrukerMedScore> KamperMedFirePoeng { get; set; }
         public IEnumerable<BrukerMedScore> KamperMedPoeng { get; set; }
+        public IEnumerable<KampMedScore> KampMedFlestPoeng { get; set; }
+        public IEnumerable<KampMedScore> KampMedFærrestPoeng { get; set; }
+    }
+
+    public class KampMedScore
+    {
+        public string Lag { get; set; }
+        public int Score { get; set; }
     }
 
     public class BrukerMedScore
